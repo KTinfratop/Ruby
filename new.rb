01887d8ffff3be0a8c635dc5d2dd45c0a8706df0
EC2 Default User <ec2-user@ip-172-31-29-191.ap-northeast-1.compute.internal>
2023-09-04 02:23:49 +0000

    [finish]2-9

--- new.rb
+++ new.rb
@@ -1,5 +1,12 @@
+# インスタンスメソッド
 class Car
-  
+
+  def move(direction, distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+
+
   def turn(direction)
     puts "#{direction}に曲がります。"
   end
@@ -15,3 +22,21 @@ car.turn("右")
 
 car = Car.new
 car.run(5)
+
+# クラスメソッド
+
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+Car.run(10)
+
+class Car
+  def self.turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+end
+
+Car.turn("右")
